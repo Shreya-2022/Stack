f03f7258430435dfdc662cfb564d9cd77cd0918f
Shreya Tiwari <117257467+Shreya-2022@users.noreply.github.com>
2024-08-28 22:18:28 +0530

    Stack creation insertation

--- stackcreation.cpp
+++ stackcreation.cpp
@@ -1,63 +1,27 @@
 #include <iostream>
+#include<stack>
 using namespace std;
-int stack[100], n=100, top=-1;
-void push(int val) {
-   if(top>=n-1)
-   cout<<"Stack Overflow"<<endl;
-   else {
-      top++;
-      stack[top]=val;
+int main(){
+   //creation
+   stack<int>st;
+
+   //insertation 
+   st.push(10);
+   st.push(20);
+   st.push(30);
+// size check
+   cout<< "size of stack is:" <<st.size() << endl;
+   // remove
+   st.pop();
+   // check empty
+   if(st.empty()){
+      cout<< "Stack is empty"<< endl;
    }
-}
-void pop() {
-   if(top<=-1)
-   cout<<"Stack Underflow"<<endl;
-   else {
-      cout<<"The popped element is "<< stack[top] <<endl;
-      top--;
+   else{
+      cout<<"Stack is not empty"<<endl;
    }
-}
-void display() {
-   if(top>=0) {
-      cout<<"Stack elements are:";
-      for(int i=top; i>=0; i--)
-      cout<<stack[i]<<" ";
-      cout<<endl;
-   } else
-   cout<<"Stack is empty";
-}
-int main() {
-   int ch, val;
-   cout<<"1) Push in stack"<<endl;
-   cout<<"2) Pop from stack"<<endl;
-   cout<<"3) Display stack"<<endl;
-   cout<<"4) Exit"<<endl;
-   do {
-      cout<<"Enter choice: "<<endl;
-      cin>>ch;
-      switch(ch) {
-         case 1: {
-            cout<<"Enter value to be pushed:"<<endl;
-            cin>>val;
-            push(val);
-            break;
-         }
-         case 2: {
-            pop();
-            break;
-         }
-         case 3: {
-            display();
-            break;
-         }
-         case 4: {
-            cout<<"Exit"<<endl;
-            break;
-         }
-         default: {
-            cout<<"Invalid Choice"<<endl;
-         }
-      }
-   }while(ch!=4);
-   return 0;
+
+   //top element
+   cout<<st.top()<<endl;
+return 0 ;
 }
